af513dfc5e99024e6b67b424269920fd00eb4252
Steven He <therealonestig@gmail.com>
2024-12-08 17:33:26 -0800

    constraint satisfaction

--- makepdf.sh
+++ makepdf.sh
@@ -1,1 +1,5 @@
-pdflatex final.tex && rm -f final.aux final.log final.fls final.fdb_latexmk final.synctex.gz
+pdflatex -interaction=nonstopmode final.tex | tee pdflatex_output.log | grep -A 5 -E "^(.*Error:|!.*)"
+if [ ${PIPESTATUS[0]} -ne 0 ]; then
+    echo "uh oh"
+fi
+rm -f final.aux final.log final.fls final.fdb_latexmk final.synctex.gz pdflatex_output.log
